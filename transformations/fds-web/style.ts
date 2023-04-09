87a20dab84e9e8ea24d492a02c5846127c495f14
CatGrandi <53832992+CatGrandi@users.noreply.github.com>
2023-04-09 17:16:22 -0400

    Create file: "transformations/fds-web/style.ts"

---
+++ transformations/fds-web/style.ts
@@ -0,0 +1,37 @@
+/* eslint-disable */
+import { PRYProp } from "./css-atoms";
+import type * as CSS from 'csstype';
+
+/**
+ * style
+ * Function for dynamically creating and auto-completing
+ * PRY design system atoms.
+ * @param {DSysProp} dsysStyles
+ * @param {CSS.Properties} otherStyles
+ * @return {string}
+ */
+export default function style(
+  dsysStyles: PRYProp,
+  otherStyles: CSS.Properties = {},
+) : string {
+  const toKebab =
+    (str) => str.replace(/([a-z0–9])([A-Z])/g, "$1-$2").toLowerCase();
+  return `style="${
+    Object.entries(dsysStyles).map((entry) => {
+      const name = toKebab(entry[0]);
+      const value = entry[1];
+      if (value === true) {
+        return `--pry-${name}: 1;`;
+      }else if (!isNaN(value as any)) {
+        return `--pry-${name}: ${value};`;
+      }else{
+        return `--pry-${name}: var( --pry-${value} );`;
+      }
+    }).join('\n  ')}${
+    Object.entries(otherStyles).map((entry) => {
+      if (!entry[0]) return '';
+      const name = toKebab(entry[0]);
+      return `${name}: ${entry[1]};`;
+    }).join('\n  ')
+  }"`;
+}
